a5d2bed0b178dec83b1df20a8662cea054689496
Gary Gilbert <ggary104@hotmail.com>
2022-01-15 01:54:09 -0800

    added search 1/3

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,16 +1,33 @@
 from django.shortcuts import render
+from .forms import SearchForm
 
 from . import util
 
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
+    form = SearchForm(request.POST)
+    if request.method == 'GET':
+        form = SearchForm(request.POST)
+        return render(request, "encyclopedia/index.html", {
+            "form": form,
+            "entries": util.list_entries()
+        })
+    elif request.method == 'POST':
+        print('test')
+        if form.is_valid():
+            title = form.cleaned_data["search"],
+            print('test2')
+            return render(request, "encyclopedia/entry.html", {
+                "form": form,
+                "title": title[0].capitalize(),
+                "content": util.get_entry(title[0])
+            })
 
 
 def entry(request, entry):
+    form = SearchForm(request.POST)
     return render(request, "encyclopedia/entry.html", {
+        "form": form,
         "title": entry,
         "content": util.get_entry(entry)
     })
